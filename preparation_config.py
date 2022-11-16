8ff83b0edb6104f16d3f9291547c966732accf7b
Fanny Almy <faalmy@tik42x.ee.ethz.ch>
2022-11-16 22:40:44 +0100

    Added yes_no task function, minor changes in the preparator and descriptions.

--- preparation_config.py
+++ preparation_config.py
@@ -11,13 +11,14 @@ preparation_config = dict()
 # 'Direction_task' (dataset: 'dots' or 'processing_speed'):
 # 'Position_task' (dataset: 'dots'):
 # 'Segmentation_task' (dataset: 'antisaccade', 'dots', or 'processing_speed'):
-preparation_config['task'] = 'LR_task'
-preparation_config['dataset'] = 'antisaccade'
+# 'Yes_No_task' (dataset: 'wisc' and 'synchronized EEG'):
+preparation_config['task']    = 'Yes_No_task'
+preparation_config['dataset'] = 'WISC' #'antisaccade'
 
 # We provide two types of preprocessing on the dataset (minimal preprocessing and maximal preprocessing). Choices are
 # 'max'
 # 'min'
-preparation_config['preprocessing'] = 'max'  # or min
+preparation_config['preprocessing']      = 'min'  
 preparation_config['preprocessing_path'] = 'synchronized_' + preparation_config['preprocessing']
 
 # We provide also dataset where features are extracted
@@ -38,17 +39,19 @@ preparation_config['output_name'] = preparation_config['output_name'] + ('_hilbe
 ##################################################################################
 # We prepare some helper variables to locate the correct datasets and the files that we need and to use them.
 preparation_config['LOAD_ANTISACCADE_PATH'] = '../data/measured/antisaccade_task_data/' + preparation_config['preprocessing_path'] + '/'
-preparation_config['ANTISACCADE_FILE_PATTERN'] = '[go]ip_..._AS_EEG.mat'
+preparation_config['ANTISACCADE_FILE_PATTERN']         = '[go]ip_..._AS_EEG.mat'
 preparation_config['ANTISACCADE_HILBERT_FILE_PATTERN'] = '[go]ip_..._AS_EEG.mat'
 
 preparation_config['LOAD_DOTS_PATH'] = '../data/measured/dots_data/' + preparation_config['preprocessing_path'] + '/'
-preparation_config['DOTS_FILE_PATTERN'] = '(ep|EP).._DOTS._EEG.mat'
+preparation_config['DOTS_FILE_PATTERN']         = '(ep|EP).._DOTS._EEG.mat'
 preparation_config['DOTS_HILBERT_FILE_PATTERN'] = '(ep|EP).._DOTS._EEG.mat'
 
 preparation_config['LOAD_PROCESSING_SPEED_PATH'] = '../data/measured/processing_speed_data/' + preparation_config['preprocessing_path'] + '/'
-preparation_config['PROCESSING_SPEED_FILE_PATTERN'] = '..._WI2_EEG.mat'
+preparation_config['PROCESSING_SPEED_FILE_PATTERN']         = '..._WI2_EEG.mat'
 preparation_config['PROCESSING_SPEED_HILBERT_FILE_PATTERN'] = '..._WI2_EEG.mat'
 
+preparation_config['LOAD_WISC_PATH']         = '.../data/measured/'
+preparation_config['WISC_FILE_PATTERN']      = '..._W[12]_wisc_events.npz'
 
 
 ##################################################################################
@@ -59,9 +62,9 @@ preparation_config['PROCESSING_SPEED_HILBERT_FILE_PATTERN'] = '..._WI2_EEG.mat'
 ##################################################################################
 ##################################################################################
 # Internal information about each dataset (antisaccade, dots, processing_speeed)
-preparation_config['saccade_trigger'] = ['L_saccade', 'R_saccade']
+preparation_config['saccade_trigger']  = ['L_saccade', 'R_saccade']
 preparation_config['fixation_trigger'] = ['L_fixation', 'R_fixation']
-preparation_config['blink_trigger'] = ['L_blink', 'R_blink']
+preparation_config['blink_trigger']    = ['L_blink', 'R_blink']
 
 # Anti-saccade dataset
 preparation_config['antisaccade'] = dict()
